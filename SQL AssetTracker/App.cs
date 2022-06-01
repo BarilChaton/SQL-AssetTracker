76028170402d79a8668583efa640b9323815e8ef
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-06-01 10:52:44 +0200

    Added code to make console auto fullscreen.

--- SQL AssetTracker/App.cs
+++ SQL AssetTracker/App.cs
@@ -1,7 +1,30 @@
-﻿namespace SQL_AssetTracker
-{
-    internal class App
-    {
+﻿using System.Runtime.InteropServices;
 
+namespace SQL_AssetTracker
+{
+    class App
+    {
+        // Setup Fullscreen.
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
+
+
+        static void Main(string[] args)
+        {
+            // Apply Fullscreen.
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            ShowWindow(ThisConsole, MAXIMIZE);
+
+            //Run AppLogic class and method here...
+        }
     }
 }
