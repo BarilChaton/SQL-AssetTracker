f132eb966bff9416485f1091ce3787e3d2b44ac0
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-06-01 10:26:55 +0200

    Constructor finished

--- SQL AssetTracker/AssetConstructor.cs
+++ SQL AssetTracker/AssetConstructor.cs
@@ -1,13 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SQL_AssetTracker
+﻿namespace SQL_AssetTracker
 {
-    internal class AssetConstructor
+    public class AssetConstructor
     {
+        public AssetConstructor()
+        {
+        }
+
+        public AssetConstructor(int id,
+                                string office,
+                                string brand,
+                                DateTime purchaseDate,
+                                double price,
+                                string currency,
+                                string laptopModel,
+                                string phoneModel,
+                                string type,
+                                double localPrice)
+        {
+            Id = id;
+            Office = office;
+            Brand = brand;
+            PurchaseDate = purchaseDate;
+            Price = price;
+            Currency = currency;
+            LaptopModel = laptopModel;
+            PhoneModel = phoneModel;
+            Type = type;
+            LocalPrice = localPrice;
+        }
+
+        public int Id { get; set; }
+        public string Office { get; set; }
+        public string Brand { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public double Price { get; set; }
+        public string Currency { get; set; }
+        public string LaptopModel { get; set; }
+        public string PhoneModel { get; set; }
+        public string Type { get; set; }
+        public double LocalPrice { get; set; }
+
+
+
 
     }
 }
