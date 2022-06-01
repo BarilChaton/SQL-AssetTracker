cd2ecf6a250b530164c8da29e95db1c95ef85b6b
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-06-01 10:39:34 +0200

    Created the menu system.

---
+++ SQL AssetTracker/MenuSystem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using static System.Console;
+
+namespace SQL_AssetTracker
+{
+    class MenuSystem
+    {
+        private int SelectedIndex;
+        private string[] Options;
+        private string Prompt;
+
+        public MenuSystem(int selectedIndex, string[] options, string prompt)
+        {
+            SelectedIndex = 0;
+            Options = options;
+            Prompt = prompt;
+        }
+
+        public void DisplayOptions()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+                string prefix;
+
+                if (i == SelectedIndex)
+                {
+                    prefix = "*";
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = " ";
+                    ForegroundColor= ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+
+                WriteLine($"{prefix} << {currentOption} >>");
+            }
+            ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+
+            do
+            {
+                Clear();
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
+    }
+}
